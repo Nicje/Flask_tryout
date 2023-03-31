649d1691bb142dd33927454f75a677e44624fb23
Nicole Bazen <koster065@hotmail.com>
2023-03-31 18:55:09 +0200

    regel veranderd

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello world!</p>"
+    return "<p>Welkom bij mijn Flask_tryout!</p>"
 
 if __name__== "__main__":
     app.run()
